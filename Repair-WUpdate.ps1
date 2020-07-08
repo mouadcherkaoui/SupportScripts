e824350b757859677d477d586938ef6aeed53a81
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2020-07-08 14:34:04 +0100

    Update Repair-WUpdate.ps1

--- Repair-WUpdate.ps1
+++ Repair-WUpdate.ps1
@@ -1,11 +1,17 @@
     function Repair-WUpdate{
-        $bits = Get-Service -Name "bits"
-        if($bits.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running -and $bits.CanStop) { $bits.Stop() }
-        $wuauserv = Get-Service -Name "bits"
-        if($wuauserv.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running -and $wuauserv.CanStop) { $wuauserv.Stop() }        
+        Get-PSProvider -PSProvider Environment
+        $env = Get-ChildItem -Path Env:\
+        $systemroot=$env | where Name -eq "SystemRoot" | select Value
         
-        ren %systemroot%\SoftwareDistribution SoftwareDistribution.bak
-        ren %systemroot%\System32\catroot2 catroot2.bak
+        $bits = Get-Service -Name "bits"
+        if($bits.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) { $bits.Stop() }
+        
+        $wuauserv = Get-Service -Name "bits"
+        if($wuauserv.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) { $wuauserv.Stop() }        
+        
+        Rename-Item -Path "$($systemroot.Value)\SoftwareDistribution" -NewName SoftwareDistribution.bak -Force
+        Rename-Item -Path "$($systemroot.Value)\System32\catroot2" -NewName catroot2.bak -Force
+
         $bits.Start()
         $wuauserv.Start()
 
