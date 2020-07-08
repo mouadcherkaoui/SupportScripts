43a08f3b7bfe78e8193141842d92b451da88e0f3
Mouad Cherkaoui <cherkaoui.mouad@live.fr>
2020-07-08 17:12:33 +0100

    Update Repair-WUpdate.ps1

--- Repair-WUpdate.ps1
+++ Repair-WUpdate.ps1
@@ -1,7 +1,7 @@
     function Repair-WUpdate{
         Get-PSProvider -PSProvider Environment
         $env = Get-ChildItem -Path Env:\
-        $systemroot=$env | where Name -eq "SystemRoot" | select Value
+        $systemroot= $env:SystemRoot #$env | where Name -eq "SystemRoot" | select Value
         
         $bits = Get-Service -Name "bits"
         if($bits.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) { $bits.Stop(); $bits.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped); }
@@ -9,12 +9,21 @@
         $wuauserv = Get-Service -Name "wuauserv"
         if($wuauserv.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) { $wuauserv.Stop(); $wuauserv.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped); }        
         
-        Rename-Item -Path "$($systemroot.Value)\SoftwareDistribution" -NewName SoftwareDistribution.bak -Force
-        Rename-Item -Path "$($systemroot.Value)\System32\catroot2" -NewName catroot2.bak -Force
+        $cryptSvc = Get-Service -Name "cryptSvc"
+        if($cryptSvc.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) { $cryptSvc.Stop(); $cryptSvc.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped); }        
+        
+        $msiserver = Get-Service -Name "msiserver"
+        if($msiserver.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running) { $msiserver.Stop(); $msiserver.WaitForStatus([System.ServiceProcess.ServiceControllerStatus]::Stopped); }        
+        
+        
+        Rename-Item -Path "$($env:SystemRoot)\SoftwareDistribution" -NewName SoftwareDistribution.bak -Force
+        Rename-Item -Path "$($env:SystemRoot)\System32\catroot2" -NewName catroot2.bak -Force
 
         
         $bits.Start()
         $wuauserv.Start()
+        $cryptSvc.Start()
+        $msiserver.Start()
 
         DISM.exe /Online /Cleanup-image /Restorehealth
     }
